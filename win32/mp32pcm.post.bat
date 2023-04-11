e4d525f4aff380a91c19f989ac48733b1f304e7c
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-04-10 20:59:59 -0700

    Make a very different Visual Studio project for internal use.

---
+++ win32/mp32pcm.post.bat
@@ -0,0 +1,12 @@
+:: mp32pcm.vcxproj post-build 
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+set "ProjectDir=%~dp0"
+set "SolutionPath=%~1"
+set "IntDir=%~2"
+set "OutDir=%~3"
+
+attrib -R %OutDir%..\include\mp32pcm.h >nul
+copy /y %ProjectDir%mp32pcm\mp32pcm.h %OutDir%..\include\mp32pcm.h
+attrib +R %OutDir%..\include\mp32pcm.h >nul
---
+++ win32/mp32pcm.pre.bat
@@ -0,0 +1,8 @@
+:: mp32pcm.vcxproj pre-build 
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+set "ProjectDir=%~dp0"
+set "SolutionPath=%~1"
+set "IntDir=%~2"
+set "OutDir=%~3"
